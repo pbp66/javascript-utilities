147d9071ba35b7fc0baa60d89a9a485a9366d9bb
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-31 21:36:40 -0400

    Added basic settings

---
+++ packages/pomodoro/settings.js
@@ -0,0 +1,8 @@
+class Settings {
+    constructor(onTime = 1500, offTime = 300) {
+        this.onTime = onTime; // seconds
+        this.offTime = offTime; // seconds
+    }
+}
+
+module.exports = {Settings};
