ab612659a46d3ef091c1bbf131185d656433ba7c
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-10-31 21:59:20 -0400

    Added some tutorial information to bash.js

--- packages/bash/bash.js
+++ packages/bash/bash.js
@@ -1,12 +1,12 @@
 // https://stackabuse.com/executing-shell-commands-with-node-js/
-const cp = require('child_process');
+const cp = require('child_process'); // https://nodejs.org/docs/latest-v16.x/api/child_process.html
 
 class Bash {
     constructor() {
 
     }
 
-    terminal(command) {
+    #terminal(command) {
         cp.exec(command, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
@@ -19,8 +19,30 @@ class Bash {
             console.log(`stdout: ${stdout}`);
         });
     }
+
+    terminal(command, args, options) {
+        this.currentCommand = cp.spawn(command, args);
+        this.currentCommand.stdout.on("data", data => {
+            console.log(`stdout: ${data}`);
+        });
+        
+        this.currentCommand.stderr.on("data", data => {
+            console.log(`stderr: ${data}`);
+        });
+        
+        this.currentCommand.on('error', (error) => {
+            console.log(`error: ${error.message}`);
+        });
+        
+        this.currentCommand.on("close", code => {
+            console.log(`child process exited with code ${code}`);
+        });
+    }
 }
 
 // DEV TESTING SECTION
 const bash = new Bash();
-bash.terminal("ls -la");
+bash.terminal("ls", ["-la"]);
+
+// Could use instead: https://www.npmjs.com/package/shelljs
+// Another guide: https://opensource.com/article/18/7/node-js-interactive-cli
