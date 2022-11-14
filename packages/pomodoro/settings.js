d1e3d393ad32b88454b20f3c0773d3076a1f79a0
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-13 23:15:21 -0500

    Updating git work and sub trees

--- packages/pomodoro/settings.js
+++
@@ -1,8 +0,0 @@
-class Settings {
-    constructor(onTime = 1500, offTime = 300) {
-        this.onTime = onTime; // seconds
-        this.offTime = offTime; // seconds
-    }
-}
-
-module.exports = {Settings};
